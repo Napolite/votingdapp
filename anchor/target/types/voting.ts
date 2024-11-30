b31e317ffaec1d18a6871c2f7d46c3732708d12d
constantine <okonemmanuelmma@gmail.com>
2024-11-30 11:03:23 +0100

    commititng for sakes

--- anchor/target/types/voting.ts
+++ anchor/target/types/voting.ts
@@ -5,112 +5,94 @@
  * IDL can be found at `target/idl/voting.json`.
  */
 export type Voting = {
-  "address": "AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ",
-  "metadata": {
-    "name": "voting",
-    "version": "0.1.0",
-    "spec": "0.1.0",
-    "description": "Created with Anchor"
-  },
-  "instructions": [
+  address: "AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ";
+  metadata: {
+    name: "voting";
+    version: "0.1.0";
+    spec: "0.1.0";
+    description: "Created with Anchor";
+  };
+  instructions: [
     {
-      "name": "initialisePoll",
-      "discriminator": [
-        24,
-        62,
-        111,
-        85,
-        129,
-        106,
-        131,
-        175
-      ],
-      "accounts": [
+      name: "initialisePoll";
+      discriminator: [24, 62, 111, 85, 129, 106, 131, 175];
+      accounts: [
         {
-          "name": "signer",
-          "writable": true,
-          "signer": true
+          name: "signer";
+          writable: true;
+          signer: true;
         },
         {
-          "name": "poll",
-          "writable": true,
-          "pda": {
-            "seeds": [
+          name: "poll";
+          writable: true;
+          pda: {
+            seeds: [
               {
-                "kind": "arg",
-                "path": "pollId"
+                kind: "arg";
+                path: "pollId";
               }
-            ]
-          }
+            ];
+          };
         },
         {
-          "name": "systemProgram",
-          "address": "11111111111111111111111111111111"
+          name: "systemProgram";
+          address: "11111111111111111111111111111111";
         }
-      ],
-      "args": [
+      ];
+      args: [
         {
-          "name": "pollId",
-          "type": "u64"
+          name: "pollId";
+          type: "u64";
         },
         {
-          "name": "description",
-          "type": "string"
+          name: "description";
+          type: "string";
         },
         {
-          "name": "pollStart",
-          "type": "u64"
+          name: "pollStart";
+          type: "u64";
         },
         {
-          "name": "pollEnd",
-          "type": "u64"
+          name: "pollEnd";
+          type: "u64";
         }
-      ]
+      ];
     }
-  ],
-  "accounts": [
+  ];
+  accounts: [
     {
-      "name": "poll",
-      "discriminator": [
-        110,
-        234,
-        167,
-        188,
-        231,
-        136,
-        153,
-        111
-      ]
+      name: "poll";
+      discriminator: [110, 234, 167, 188, 231, 136, 153, 111];
     }
-  ],
-  "types": [
+  ];
+  types: [
     {
-      "name": "poll",
-      "type": {
-        "kind": "struct",
-        "fields": [
+      name: "poll";
+      type: {
+        kind: "struct";
+        fields: [
           {
-            "name": "pollId",
-            "type": "u64"
+            name: "pollId";
+            type: "u64";
           },
           {
-            "name": "description",
-            "type": "string"
+            name: "description";
+            type: "string";
           },
           {
-            "name": "pollStart",
-            "type": "u64"
+            name: "pollStart";
+            type: "u64";
           },
           {
-            "name": "pollEnd",
-            "type": "u64"
+            name: "pollEnd";
+            type: "u64";
           },
           {
-            "name": "candidateAmount",
-            "type": "u64"
+            name: "candidateAmount";
+            type: "u64";
           }
-        ]
-      }
+        ];
+      };
     }
-  ]
+  ];
 };
